11133478a7b64c1d158aad54405f4a7f34e49bdf
Wataru MIYAGUNI <gonngo@gmail.com>
2014-07-19 11:26:54 +0900

    Use express instead of node-static.

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
-var static = require('node-static');
-
-var fileServer = new static.Server('./public');
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 8080;
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    }).resume();
-}).listen(port);
+app.use('/', express.static(__dirname + '/public'));
 
+var server = app.listen(port, function() {
+    console.log(__dirname + '/public');
+    console.log('Listening on port %d', server.address().port);
+});
 
 if (process.env.NODETIME_ACCOUNT_KEY) {
     require('nodetime').profile({
