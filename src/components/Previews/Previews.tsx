39fa708a83851cdf700711f01af70fb053227778
Wataru MIYAGUNI <gonngo@gmail.com>
2021-07-01 01:27:15 +0900

    :lipstick: Use `TabBar` and `TabItem` from SmartHR UI

--- src/components/Previews/Previews.tsx
+++ src/components/Previews/Previews.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import { EmojiSource } from './components/EmojiSource'
 import { EmojiPreview } from './components/EmojiPreview'
-import { Base, TabBar, TabItem, defaultSpacing } from 'smarthr-ui'
+import { TabBar, TabItem, defaultSpacing } from 'smarthr-ui'
 
 type Props = {
   figletText: string
 }
 
+type TabItems = 'source' | 'preview'
+
 export const Previews: React.VFC<Props> = ({ figletText }) => {
   const [colonsText, setColonsText] = useState<string>('')
+  const [currentTab, setCurrentTab] = useState<TabItems>('preview')
   const foregroundEmojiText = ':pizza:'
   const backgroundEmojiText = ':cloud:'
 
@@ -18,16 +21,32 @@ export const Previews: React.VFC<Props> = ({ figletText }) => {
     setColonsText(figletText.replace(/\S/g, foregroundEmojiText).replace(/[^\n\S]/g, backgroundEmojiText))
   }, [figletText])
 
+  const onClickTab = useCallback((tab: TabItems) => {
+    setCurrentTab(tab)
+  }, [])
+
   return (
     <>
-      <Base>
-        <Heading type="sectionTitle">FIGlet source</Heading>
-        <EmojiSource text={colonsText} />
-      </Base>
-      <Base>
-        <Heading type="sectionTitle">Preview</Heading>
-        <EmojiPreview foreground={foregroundEmojiText} background={backgroundEmojiText} colonsText={colonsText} />
-      </Base>
+      <TabBar>
+        <TabItem id="tab-preview" selected={currentTab === 'preview'} onClick={() => onClickTab('preview')}>
+          Emoji Preivew
+        </TabItem>
+        <TabItem id="tab-source" selected={currentTab === 'source'} onClick={() => onClickTab('source')}>
+          Emoji Source
+        </TabItem>
+      </TabBar>
+      <Result>
+        {currentTab === 'source' ? (
+          <EmojiSource text={colonsText} />
+        ) : (
+          <EmojiPreview foreground={foregroundEmojiText} background={backgroundEmojiText} colonsText={colonsText} />
+        )}
+      </Result>
     </>
   )
 }
+
+const Result = styled.div`
+  height: 80vh;
+  margin: ${defaultSpacing.XS} 0;
+`
