7e49f6e0f953411b670170f8da00ef092a3ac88d
Wataru MIYAGUNI <gonngo@gmail.com>
2021-07-04 02:08:36 +0900

    :lipstick: Use Stack layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Textarea } from 'smarthr-ui'
+import styled from 'styled-components'
+
 import { useFiglet } from 'react-hook-figlet'
 import { Previews } from './components/Previews'
+import { Base, Heading, Stack, Textarea, defaultSpacing } from 'smarthr-ui'
 
 export const App: React.VFC = () => {
   const [figletText, setSourceText] = useFiglet('Old Banner')
@@ -11,9 +13,27 @@ export const App: React.VFC = () => {
   }
 
   return (
-    <>
-      <Textarea name="sourceText" onChange={handleChangeSourceText} />
-      <Previews figletText={figletText} />
-    </>
+    <Container>
+      <Stack gap="M">
+        <Heading>Pizza FIGLet</Heading>
+        <Content>
+          <Textarea width="100%" onChange={handleChangeSourceText} />
+        </Content>
+        <Content>
+          <Previews figletText={figletText} />
+        </Content>
+      </Stack>
+    </Container>
   )
 }
+
+const Content = styled(Base)`
+  padding: ${defaultSpacing.S};
+`
+
+const Container = styled.div`
+  margin-top: ${defaultSpacing.M};
+  width: 1024px;
+  margin-right: auto;
+  margin-left: auto;
+`
